515d196c4c47f73219718d7e986cc13862613ddd
Ayberk Bayam <bayam20cp@gmail.com>
2026-03-20 19:24:34 +0300

    feat: new information has been added

--- 101/encapsulation_main.dart
+++ 101/encapsulation_main.dart
@@ -2,13 +2,20 @@ import 'encapsulation_class.dart';
 
 void main() {
   try {
-    var p = BankEncapsulationClass(
+    var p1 = BankEncapsulationClass(
       id: 1,
       firstName: 'John',
       lastName: 'Doe',
       price: 450.00,
     );
-    p.printInfo();
+    var p2 = BankEncapsulationClass(
+      id: 2,
+      firstName: 'Frank',
+      lastName: 'Guerrero',
+      price: 450.00,
+    );
+    p1.printInfo();
+    p2.printInfo();
   } catch (e) {
     print('Error');
   }
